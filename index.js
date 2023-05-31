15d90dd4f25140785bf6d5225eb964f87f21d621
amanchn123 <118670647+amanchn123@users.noreply.github.com>
2023-05-31 16:32:02 +0530

    aa
    aaa

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname,'./client/build')))
 
 app.get("*",(req,resp)=>{
-    resp.sendFile(path.join('.client//build/index.html'))
+    resp.sendFile(__dirname,(path.join('.client//build/index.html')))
 })
 
 
