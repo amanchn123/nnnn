03c0f2faf7355b75db839136a62d444987002733
amanchn123 <118670647+amanchn123@users.noreply.github.com>
2023-06-14 10:51:09 +0530

    ssss
    ssss

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express=require('express')
 const con=require('./connection')
 const cors=require('cors')
 const dotenv=require('dotenv')
+
 // const usernotfound=require('./CHATTING-section/errorHandling/apinotfound')
 const register=require('./socialMedia-section/routes/AuthRoute.js')
-const verify =require('./socialMedia-section/JWT_verification')
+
+const usernotfound=require('./CHATTING-section/errorHandling/apinotfound')
+
+
 const app=express()
 const path=require('path')
 dotenv.config()
@@ -12,6 +16,7 @@ con()
 app.use(cors({origin:"*"}))
 
 app.use(express.json())
+
 
 app.use(express.static(path.join(__dirname,'./client/build')))
 
@@ -21,10 +26,27 @@ app.get("*",(req,resp)=>{
 
 
 // app.use('/api',require('./CHATTING-section/useroutes/userRoute'))
+
+// app.use(express.static(path.join(__dirname,'../social-frontend/build')))
+
+// app.get("*",(req,resp)=>{
+//     resp.sendFile(path.join('../social-frontend/build/index.html'))
+// })
+
+
+
+
+
+app.use('/api',require('./CHATTING-section/useroutes/userRoute'))
+
 app.use('/apii',require('./socialMedia-section/routes/userRoutes'))
 app.use('/apii',register)
 
 app.use('/apii',require('./socialMedia-section/routes/postRoutes'))
 
 app.use('/you',require('./socialMedia-section/routes/you'))
+
 app.listen(5000 || process.env.PORT ,console.log('running succesfully'))
+
+app.listen(process.env.PORT || 4000,console.log('running succesfully'))
+
--- socialMedia-section/routes/postRoutes.js
+++ socialMedia-section/routes/postRoutes.js
@@ -1,17 +1,29 @@
 const express=require('express')
 const router=express.Router()
-const {CreatePost,getPost,updatePost,deletePost,LikePost,timeLinePost,Createstories,currentUserstory,storyTimeline}= require('../PostController')
+
+const {CreatePost,getPost,updatePost,deletePost,LikePost,timeLinePost,Createstories,currentUserstory,storyTimeline,youtube}= require('../PostController')
+
+
 const verify=require('../JWT_verification')
 
 router.post('/newPost',verify,CreatePost)
 router.put('/likes/',verify,LikePost)
 router.get('/getpost',getPost)
+
+router.get('/getpost',verify,getPost)
+
 router.put('/updatepost',updatePost)
 router.post('/deletepost',verify,deletePost)
 router.get('/timelinePost',verify,timeLinePost)
 router.post('/createStory',Createstories)
 router.get('/getmystory',currentUserstory)
 router.get('/stories',storyTimeline)
+
+
+
+module.exports=router
+
+router.post('/you',youtube)
 
 
 module.exports=router
